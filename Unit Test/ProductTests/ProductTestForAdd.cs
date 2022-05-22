eb4dd49aad92c968fa0aa7dd1e004ab8373b24af
berk cinaz <berk.cinaz@clockwork.com.tr>
2022-05-22 19:24:10 +0300

    implemented unit test

---
+++ Unit Test/ProductTests/ProductTestForAdd.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Business.ValidationAspect.FluentValidation;
+using Entities.Concrete;
+using Entities.Dtos.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Unit.Test.ProductTests
+{
+    public class ProductTestForAdd
+    {
+        IProductService _productService;
+        ProductForAddValidator validationRules;
+        public ProductTestForAdd(IProductService productService)
+        {
+            _productService = productService;
+            validationRules = new ProductForAddValidator();
+        }
+        [Fact]
+        public void ProductAddTest()
+        {
+            var productForAdd = new ProductForAddDto()
+            {
+                BrandId = 1,
+                Description = "description of product",
+                Name = "name of product",
+                ProductCode = "TEST0101PRODUCT",
+                QuantityPerUnit = "ADET",
+                UnitsInStock = 23,
+                UnitPrice = 54,
+            };
+            var result = validationRules.Validate(productForAdd);
+            Assert.True(result.Errors.Any());
+        }
+    }
+}
---
+++ Unit Test/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Unit_Test
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())//Microsoft.Extensions.Configuration.Json
+                    .AddJsonFile("appsettings.json", false, true)
+                    .Build();
+
+            services.AddScoped<IProductDal, EfProductDal>();
+            services.AddScoped<IProductService, ProductManager>();
+        }
+    }
+}
